7d472c28d694451c420778b730777dc7ee028c0e
Pasindu Sathsara <pasindusathsara@Pasindus-MacBook-Pro.local>
2026-07-13 23:24:01 +0530

    initial base of project

---
+++ src/main/java/com/zage/zage/ZageApplication.java
@@ -0,0 +1,13 @@
+package com.zage.zage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zage/zage/ZageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zage.zage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
